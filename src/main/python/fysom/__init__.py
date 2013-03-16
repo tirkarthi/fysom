af20a85bb444af3c1c15c31ad16bc120861d45a6
mriehl <maximilien.riehl@gmail.com>
2013-03-16 18:07:26 +0100

    trying to get coverage to 100% by providing alternative backwards compatibility

--- src/main/python/fysom/__init__.py
+++ src/main/python/fysom/__init__.py
@@ -26,6 +26,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+from __future__ import unicode_literals
 
 __author__ = 'Mansour Behabadi'
 __copyright__ = 'Copyright 2011, Mansour Behabadi and Jake Gordon'
@@ -34,12 +35,6 @@ __license__ = 'MIT'
 __version__ = '${version}'
 __maintainer__ = 'Mansour Behabadi'
 __email__ = 'mansour@oxplot.com'
-
-try:
-    unicode = unicode
-except NameError:
-    unicode = str
-    basestring = (str, bytes)
 
 
 class FysomError(Exception):
