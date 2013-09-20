3dd956a364657c9aae0c110f0a27fad43eb8333a
mriehl <maximilien.riehl@gmail.com>
2013-09-20 09:49:58 +0200

    releasing 1.0.12

--- src/main/python/fysom/__init__.py
+++ src/main/python/fysom/__init__.py
@@ -31,7 +31,7 @@ __author__ = 'Mansour Behabadi'
 __copyright__ = 'Copyright 2011, Mansour Behabadi and Jake Gordon'
 __credits__ = ['Mansour Behabadi', 'Jake Gordon']
 __license__ = 'MIT'
-__version__ = '1.0.11'
+__version__ = '${version}'
 __maintainer__ = 'Mansour Behabadi'
 __email__ = 'mansour@oxplot.com'
 
