f884d66f99689e3f874d0164cb1283e20c5a3744
Stuart Longland <me@vk4msl.yi.org>
2017-03-14 20:18:43 +1000

    Unit tests: Add test checking `unicode_literals`

---
+++ src/unittest/python/test_unicode_literals.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+#
+# fysom - pYthOn Finite State Machine - this is a port of Jake
+#         Gordon's javascript-state-machine to python
+#         https://github.com/jakesgordon/javascript-state-machine
+#
+# Copyright (C) 2011 Mansour Behabadi <mansour@oxplot.com>, Jake Gordon
+#                                        and other contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+from __future__ import unicode_literals
+
+import unittest
+from fysom import Fysom
+
+
+class FysomUnicodeLiterals(unittest.TestCase):
+    def test_it_doesnt_break_with_unicode_literals(self):
+        try:
+            fsm = Fysom({
+                'initial': 'green',
+                'final': 'red',
+                'events': [
+                    {'name': 'warn', 'src': 'green', 'dst': 'yellow'},
+                    {'name': 'panic', 'src': 'yellow', 'dst': 'red'},
+                    {'name': 'calm', 'src': 'red', 'dst': 'yellow'},
+                    {'name': 'clear', 'src': 'yellow', 'dst': 'green'}
+                ]
+            })
+            self.assertTrue(True)
+        except TypeError:
+            self.assertTrue(False)
